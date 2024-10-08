cd13a805e32e7045b0fb9e2885ff5b7eef71ef4f
guSElias <gustavoeliasdesouza2310@gmail.com>
2024-10-08 15:59:15 -0300

    Login modal improvements

--- src/components/loginModal/LoginModal.vue
+++ src/components/loginModal/LoginModal.vue
@@ -1,13 +1,23 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, watch } from 'vue';
+import { useQuasar } from 'quasar';
 
+const $q = useQuasar();
+
+const isDark = ref($q.dark.isActive);
+
+watch(
+  () => $q.dark.isActive,
+  (val) => {
+    isDark.value = val;
+  }
+);
 const form = ref({
   email: '',
   password: '',
 });
-
+const showpassword = ref(false);
 const showDialog = ref(false);
-
 function openDialog() {
   showDialog.value = true;
 }
@@ -18,17 +28,47 @@ defineExpose({
 </script>
 <template>
   <q-dialog backdrop-filter="blur(4px)" v-model="showDialog">
-    <q-card style="width: 500px; max-width: 80vw">
-      <q-card-section class="row justify-center">
-        <q-icon name="img:src/assets/ingressou-logo.svg" size="10rem" />
+    <q-card style="width: 400px; max-width: 80vw">
+      <q-card-section
+        class="row justify-center q-pa-none"
+        style="max-height: 8rem"
+      >
+        <q-icon
+          v-if="isDark"
+          name="img:src/assets/ingressou-logo-dark.svg"
+          size="10rem"
+          height="120"
+        />
+        <q-icon
+          v-else
+          name="img:src/assets/ingressou-logo.svg"
+          size="10rem"
+          height="120"
+        />
         <q-btn icon="close" flat color="primary" v-close-popup />
       </q-card-section>
-      <q-card-section class="column justify-center">
-        <div class="text-h5 text-center">Login</div>
-        <q-form class="row justify-center">
-          <div class="col-md-4 col-sm-6 col-xs-10">
-            <q-input label="Email" v-model="form.email" />
-            <q-input label="Senha" v-model="form.password" />
+      <q-card-section class="column justify-center q-pt-none">
+        <div class="text-h5 text-center text-primary">Login</div>
+        <q-form class="justify-center q-mx-xl">
+          <div class="col-md-4 col-sm-6 col-xs-10 q-pb-md">
+            <q-input label="Email" v-model="form.email" type="email" />
+            <q-input
+              label="Senha"
+              v-model="form.password"
+              :type="showpassword ? 'text' : 'password'"
+            >
+              <template v-slot:append>
+                <q-icon
+                  :name="showpassword ? 'visibility' : 'visibility_off'"
+                  class="cursor-pointer"
+                  @click="showpassword = !showpassword"
+                />
+              </template>
+            </q-input>
+            <div class="q-pt-md q-gutter-md row justify-end">
+              <q-btn label="cadastrar" type="reset" color="primary" outline />
+              <q-btn label="entrar" type="submit" color="primary" />
+            </div>
           </div>
         </q-form>
       </q-card-section>
--- src/components/loginModal/types.ts
+++
@@ -1,3 +0,0 @@
-export default interface LoginModalProps {
-  isDark: boolean
-}
